3292906dbf3028eb68b224e2f8e4e338799a54bb
Jessica Sommerville <jessica.c.sommerville@gmail.com>
2023-07-12 17:17:15 -0700

    Add JS solution

--- javascript/fibonacci.js
+++ javascript/fibonacci.js
@@ -1,5 +1,17 @@
 function fibonacci(num) {
   // type your code here
+
+  if (num < 2){
+    return num
+  }
+
+  last_two = [0, 1]
+
+  for (let i=2; i <= num; i++){
+    sum = last_two[0] + last_two[1]
+    last_two = [last_two[1], sum]
+  }
+  return last_two[1]
 }
 
 if (require.main === module) {
@@ -22,3 +34,4 @@ module.exports = fibonacci;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
